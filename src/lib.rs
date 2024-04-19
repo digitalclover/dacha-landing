671e59557b7f0201b37567f42e446d1c2f04857c
Joshua McCarthy <joshua.f.mccarthy@gmail.com>
2024-04-20 00:31:17 +0900

    Added static site hosting with en.html as default

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 use std::net::SocketAddr;
 use warp::{Error, Filter, Future};
 
+const SITE_FOLDER: &str = "public";
+
 pub fn run() -> Result<(SocketAddr, impl Future<Output = ()> + 'static), Error> {
-    let hello = warp::path("health_check").map(warp::reply);
-    warp::serve(hello).try_bind_ephemeral(([127, 0, 0, 1], 0))
+    let health_check = warp::path("health_check").and(warp::get()).map(warp::reply);
+
+    let assets = warp::fs::dir(SITE_FOLDER);
+    let index = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file(format!("{}/en.html", SITE_FOLDER)));
+    let site = assets.or(index);
+    let routes = health_check.or(site);
+    warp::serve(routes).try_bind_ephemeral(([127, 0, 0, 1], 0))
 }
